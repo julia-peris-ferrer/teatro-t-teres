4c69bd0d75a30830e7926ffaba527e7a04fc3689
Julia Peris Ferrer <JULI@MacBook-Pro-de-Julia-2.local>
2018-06-02 12:40:08 +0200

    ajustes

--- src/js/advertisement.js
+++ src/js/advertisement.js
@@ -1,4 +1,4 @@
-//Cuando haces click en el botton de cerrar, cierra el anuncio de subscripción.
+//Cuando haces click en el botón de cerrar, cierra el anuncio de subscripción.
 
 
 import $ from './jquery-3.3.1.min';
